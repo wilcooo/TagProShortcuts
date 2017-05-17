61aa3f864c25a43a0dc94520c3b8652dcdd8becd
wilcooo <wverhf+github@gmail.com>
2017-05-17 22:23:26 +0200

    BugFix
    Disabled this script while playing, becuase it wouldn't work and you can't chat with P and V

--- tps.user.js
+++ tps.user.js
@@ -3,9 +3,7 @@
 // @version      0.1
 // @description  Starting a game is even simpler now.
 // @downloadURL  https://github.com/wilcooo/TagProShortcuts/raw/master/tps.user.js
-// @include      http://tagpro-*.koalabeast.com:*
-// @include      http://tangent.jukejuice.com:*
-// @include      http://*.newcompte.fr:*
+
 // @include      http://tagpro-*.koalabeast.com/
 // @include      http://tangent.jukejuice.com/
 // @include      http://*.newcompte.fr/
